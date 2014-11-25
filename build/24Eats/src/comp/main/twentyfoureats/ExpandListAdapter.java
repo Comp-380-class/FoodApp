cd4b4818a588598b821d08e1efc809056d843c73
mverplank <mary@verplankcreative.com>
2014-11-24 22:03:36 -0800

    refresh mostly fixed

--- build/24Eats/src/comp/main/twentyfoureats/ExpandListAdapter.java
+++ build/24Eats/src/comp/main/twentyfoureats/ExpandListAdapter.java
@@ -2,6 +2,7 @@ package comp.main.twentyfoureats;
 
 import placesAPI.Place;
 import android.app.Activity;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,6 +30,21 @@ public class ExpandListAdapter extends BaseExpandableListAdapter {
     this.mainControl = ((GlobalApplication) activity.getApplication()).mainControl;
   }
 
+  static class ViewHolder {
+	    public TextView text;
+  }
+  
+  static class ViewHolderChild {
+	    public TextView text;
+	    public TextView hours;
+	    public TextView time;
+	    public TextView phone;
+	    public TextView address;
+	    public TextView rating;
+	    public TextView price;
+	    public TextView web;
+}
+  
   @Override
   public Object getChild(int groupPosition, int childPosition) {
     return 0;
@@ -44,12 +60,45 @@ public class ExpandListAdapter extends BaseExpandableListAdapter {
 		      boolean isLastChild, View convertView, ViewGroup parent) {
     
 	  Place item = (Place) getGroup(groupPosition);
-    //Place detail = this.mainControl.getListOfResteraunts(context, currentLocation, callback, options)
+	  
+	  
+	  View rowView = convertView;
+	    // reuse views
+	    if (rowView == null) {
+	    	Log.i("click","inflate");
+	      LayoutInflater inflater = activity.getLayoutInflater();
+	      rowView = inflater.inflate(R.layout.panel_view, null);
+	      // configure view holder
+	      ViewHolderChild viewHolder = new ViewHolderChild();
+	      //viewHolder.time = (TextView) rowView.findViewById(R.id.tilClose);
+	      //viewHolder.hours = (TextView) rowView.findViewById(R.id.hours);
+	      viewHolder.address = (TextView) rowView.findViewById(R.id.address);
+	      viewHolder.phone = (TextView) rowView.findViewById(R.id.phone);
+	      viewHolder.rating = (TextView) rowView.findViewById(R.id.rating);
+	      viewHolder.price = (TextView) rowView.findViewById(R.id.price);
+	      viewHolder.web = (TextView) rowView.findViewById(R.id.website);
+	      rowView.setTag(viewHolder);
+	    }
+
+	    // fill data
+	    ViewHolderChild holder = (ViewHolderChild) rowView.getTag();
+	  //holder.text.setText(item.getHours());
+	  //holder.text.setText(item.timeUntilClose());
+	    //holder.text.setText(item.getName());
+	    holder.address.setText(item.getAddress());
+	    holder.phone.setText(item.getPhone());
+	    holder.rating.setText(item.getRating()+"");
+	    holder.price.setText(item.getPrice()+"");
+	    holder.web.setText(item.getWebsite());
+	    this.notifyDataSetChanged();
+/*
+	    
     //item.addDetails("1412 Campus Rd, Los Angeles, CA 90042", "323-702-8975", "http://www.url.com", 4, 2);
 	 TextView text = null;
     if (convertView == null) {
       convertView = inflater.inflate(R.layout.panel_view, null);
     }
+
 
     //text = (TextView) convertView.findViewById(R.id.hours);
     //text.setText(item.getHours());
@@ -66,7 +115,8 @@ public class ExpandListAdapter extends BaseExpandableListAdapter {
     text = (TextView) convertView.findViewById(R.id.website);
     text.setText(item.getWebsite());
     this.notifyDataSetChanged();
-    return convertView;
+    return convertView;*/
+	    return rowView;
   }
 
   @Override
@@ -109,13 +159,33 @@ public class ExpandListAdapter extends BaseExpandableListAdapter {
   @Override
   public View getGroupView(int groupPosition, boolean isExpanded,
       View convertView, ViewGroup parent) {
-    if (convertView == null) {
+	  Place group = (Place) getGroup(groupPosition);
+	  View rowView = convertView;
+	    // reuse views
+	    if (rowView == null) {
+	      LayoutInflater inflater = activity.getLayoutInflater();
+	      rowView = inflater.inflate(R.layout.button_only_view, null);
+	      // configure view holder
+	      ViewHolder viewHolder = new ViewHolder();
+	      viewHolder.text = (TextView) rowView.findViewById(R.id.name);
+	      rowView.setTag(viewHolder);
+	    }
+
+	    // fill data
+	    ViewHolder holder = (ViewHolder) rowView.getTag();
+	    //String s = names[position];
+	    holder.text.setText(group.getName());
+
+	  
+	  
+   /* if (convertView == null) {
       convertView = inflater.inflate(R.layout.button_only_view, null);
     }
     Place group = (Place) getGroup(groupPosition);
     TextView text = (TextView) convertView.findViewById(R.id.name);
     text.setText(group.getName());
-    return convertView;
+    return convertView;*/
+	    return rowView;
   }
 
   @Override
